6c02bc5857813eb28ff1de9da2c98e7449655760
Alec Tatton <alectatton1@gmail.com>
2023-08-27 18:27:07 -0600

    start db

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import * as z from "zod";
 import { ChangeEvent, useState } from "react"
 import { isBase64Image } from "@/lib/utils"
 import { useUploadThing } from "@/lib/uploadthing"
+import { updateUser } from "@/lib/actions/user.actions"
+import { usePathname, useRouter } from "next/navigation"
 
 interface Props {
     user: {
@@ -37,6 +39,8 @@ interface Props {
 const AccountProfile = ({ user, btnTitle}: Props) => {
     const [files, setfiles] = useState<File[]>([])
     const { startUpload } = useUploadThing("media");
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -72,7 +76,7 @@ const AccountProfile = ({ user, btnTitle}: Props) => {
         }
     }
 
-    const onSubmit= async (values: z.infer<typeof UserValidation>) => {
+    const onSubmit = async (values: z.infer<typeof UserValidation>) => {
         const blob = values.profile_photo;
 
         const hasImageChanged = isBase64Image(blob);
@@ -85,7 +89,19 @@ const AccountProfile = ({ user, btnTitle}: Props) => {
             }
         }
 
-        // TODO: Update user profile
+        await updateUser({
+            userId: user.id,
+            username: values.username,
+            name: values.name,
+            bio: values.bio,
+            image: values.profile_photo,
+            path: pathname,
+        });
+
+        if(pathname === '/profile/edit') {
+            router.back();
+        }   
+        router.push('/')
     }
     
     return (
